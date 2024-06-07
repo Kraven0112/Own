6f7318c40da2a6148b06f0f2e9d08450960f0b6c
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-07 20:14:03 +0545

    lies

---
+++ 7_June/Check/script.js
@@ -0,0 +1,32 @@
+//checking using operators
+console.log("Program begins")
+console.log("---------------")
+const x = Math.min() > Math.max() //true
+const y = Math.min() < Math.max() //false
+console.log(x, y)
+
+console.log("---------------")
+
+console.log(NaN === NaN) //false
+console.log(undefined === undefined) // true
+console.log(null === null) //true
+console.log(" " === " ") //true
+
+console.log("---------------")
+
+console.log(true === true) //false
+console.log(false === false) //true
+
+console.log("---------------")
+
+const a = 5
+console.log(a.toString())//into string
+
+console.log("---------------")
+
+const fruit="orange"
+console.log(fruit === fruit.toLocaleLowerCase()) // true
+
+console.log("---------------")
+
+console.log("Program end")
