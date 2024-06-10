7e1ca327dfe14f672ebebe5c1e92aebd7cc424bd
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-10 07:53:48 +0545

    lies

---
+++ 10_June/Stopwatch/script.js
@@ -0,0 +1,7 @@
+const show = document.getElementById('show')
+const start = document.getElementById('start')
+const stop = document.getElementById('stop')
+const reset = document.getElementById('reset')
+(
+start.addEventListener('click',(()=>{
+})))
