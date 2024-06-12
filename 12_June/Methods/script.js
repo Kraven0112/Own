dde459447ac5d02aa2bb7ffed5cc6bb81c8aed46
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-12 20:23:07 +0545

    lies

---
+++ 12_June/Methods/script.js
@@ -0,0 +1,15 @@
+//Using padStart() and padEnd()
+console.log( "7645".padStart(12,"*")) //output --********7645 
+console.log( "7645".padEnd(12,"*")) //output -- 7645********
+
+//Using parseInt() method
+console.log(parseInt("0255400"))//output -- 255400
+console.log(parseInt('abc')) // output -- NaN
+console.log(parseInt('0FF',16)) //output--255
+console.log(parseInt('0AA',16))//output--170
+console.log(parseInt('0BB',16))//output--187
+console.log(parseInt('0CC',16))//output --204
+console.log(parseInt('0DD',16)) //output --221
+console.log(parseInt('0EE',16))//output -- 238
+
+//usng 
