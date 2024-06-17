661b7a8a551bb1eb41b48369351ab4a2da09883c
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-06-17 11:14:36 +0545

    lies

---
+++ 17_June/replace_method/script.js
@@ -0,0 +1,11 @@
+//using replace() method
+const str1 = "Hello World"
+console.log(str1.replace("H","K"))
+
+//using replaceAll() method
+const str2  = "The giant panda, also known as the panda bear or simply panda, is a bear species endemic to China. It is characterised by its black-and-white coat and rotund body."
+
+
+console.log(str2.replaceAll("a","e"))
+console.log(str2.replaceAll("a","p"))
+console.log(str2.replaceAll("a","i"))
