35a5cc83ce472baa417c674b9ccd050304212f7e
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-07-12 14:55:54 +0530

    lies

---
+++ Simple_project2/Folder_1/script.js
@@ -0,0 +1,16 @@
+const basciPack = document.querySelector('.btn-1')
+const standardPack = document.querySelector('.btn-2')
+const premiumPack = document.querySelector('.btn-3')
+
+basciPack.addEventListener('click',(event)=>{
+    console.log(event.target)
+    window.location.href="https://www.netflix.com/signup/planform"
+})
+standardPack.addEventListener('click',(event)=>{
+    console.log(event.target)
+    window.location.href="https://www.netflix.com/signup/planform"
+})
+premiumPack.addEventListener('click',(event)=>{
+    console.log(event.target)
+    window.location.href="https://www.netflix.com/signup/planform"
+})
