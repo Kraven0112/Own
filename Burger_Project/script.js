ac5770ce69b0564d73e49f0209090905dbf44b9d
unknown <sushilChaudhary2059@gmail.com>
2024-07-31 22:48:51 +0530

    lies

--- Burger_Project/script.js
+++ Burger_Project/script.js
@@ -26,11 +26,3 @@ menuBtn.addEventListener('click',(event)=>{
         menuItem.style.visibility = "hidden"
     }
 })
-
-
-let link = document.createElement('link')
-link.type="text/css"
-link.rel = "stylesheet"
-link.href = "/style.css"
-
-document.querySelector('head').appendChild(link)
