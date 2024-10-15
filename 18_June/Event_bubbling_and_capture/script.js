eaa5d09bf9bda4256bc3f5604ec63e277cd02c3e
Kraven0112 <sushilChaudhary2059@gmail.com>
2024-10-15 13:18:39 +0530

    change

--- 18_June/Event_bubbling_and_capture/script.js
+++ 18_June/Event_bubbling_and_capture/script.js
@@ -5,25 +5,26 @@ const bigChild = document.querySelector('#bigger')
 
 window.addEventListener('click',(event)=>{
     console.log("window event listener")
-})
+},true)
+
 
 document.addEventListener('click',(event)=>{
-    console.log("sdocument event listener")
-})
+    console.log("document event listener")
+},true)
 
 document.body.addEventListener('click',(event)=>{
     console.log("body event listener")
-})
+},true)
 
 parent.addEventListener('click',(event)=>{
     console.log("parent event listener")
-})
+},true)
 
 bigChild.addEventListener('click',(event)=>{
     console.log("bigChild event listener")
-})
+},true)
 
 smallChild.addEventListener('click',(event)=>{
     console.log("smallChild event listener")
-})
+},true)
 
